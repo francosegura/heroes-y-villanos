e51803ee9770505c7d84da2985dea0f354816ee3
FedePucci1 <fedepucci80@gmail.com>
2023-11-04 15:21:15 -0300

    V1.0

--- src/batalla/Batalla.java
+++ src/batalla/Batalla.java
@@ -5,10 +5,13 @@ import personajes.Personaje;
 
 public class Batalla {
 
+	//NO PUEDEN SER DEL MISMO TIPO, TIENEN Q SER SI O SI HEROES VS VILLANOS
+	//YA SEA LIGAS O PERSONAJES
+	//personaje vs personaje
 	public static Personaje determinarGanador1v1(Personaje personaje1, Personaje personaje2, String caracteristica) {
 		String[] orden = { "Velocidad", "Fuerza", "Resistencia", "Destreza" };
 
-		// Determinar el índice de la característica actual
+		// Determinar el Ã­ndice de la caracterÃ­stica actual
 		int i = -1;
 		for (int j = 0; j < orden.length; j++) {
 			if (caracteristica.equals(orden[j])) {
@@ -18,10 +21,10 @@ public class Batalla {
 		}
 
 		if (i == -1) {
-			throw new IllegalArgumentException("Característica no válida");
+			throw new IllegalArgumentException("CaracterÃ­stica no vÃ¡lida");
 		}
 
-		// Comparar las características de los personajes en el orden establecido
+		// Comparar las caracterÃ­sticas de los personajes en el orden establecido
 		for (int k = 0; k < orden.length; k++) {
 			double caracteristica1 = personaje1.getCaracteristicas().getCaracteristica(orden[i]);
 			double caracteristica2 = personaje2.getCaracteristicas().getCaracteristica(orden[i]);
@@ -32,12 +35,13 @@ public class Batalla {
 				return personaje2;
 			}
 
-			// Si hay empate, pasa a la siguiente característica
+			// Si hay empate, pasa a la siguiente caracterÃ­stica
 			i = (i + 1) % orden.length;
 		}
 		return null;
 	}
 	
+	//personaje vs liga
 	public static Object determinarGanador1vN(Personaje personaje, Liga liga, String caracteristica) {
 		if("Velocidad" == caracteristica)
 			return personaje;
@@ -45,6 +49,7 @@ public class Batalla {
 			return liga;
 	}
 	
+	//liga vs liga
 	public static Liga determinarGanadorNvN(Liga liga1, Liga liga2, String caracteristica) {
 		return liga1;
 	}
